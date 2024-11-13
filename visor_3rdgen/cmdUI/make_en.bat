2622c7808260f04ef8402f90b277cf57ab50fa93
Franck Lesage <140308388+Effervecreanet@users.noreply.github.com>
2024-11-13 14:25:17 +0100

    name output with FR and EN

--- visor_3rdgen/cmdUI/make_en.bat
+++ visor_3rdgen/cmdUI/make_en.bat
@@ -1,3 +1,3 @@
 rc resource.rc
-cl Visor3rdGenGenGCODE.cpp Visor3rdGenMain.cpp /link resource.res /Fe:Visor3rdGen_GenGCODE.exe
+cl Visor3rdGenGenGCODE.cpp Visor3rdGenMain.cpp /link resource.res /Fe:Visor3rdGen_GenGCODE_EN.exe
 del *.obj *.res
--- visor_3rdgen/cmdUI/make_fr.bat
+++ visor_3rdgen/cmdUI/make_fr.bat
@@ -1,3 +1,3 @@
 rc resource.rc
-cl Visor3rdGenGenGCODE.cpp Visor3rdGenMain.cpp /D LANG_FR /link resource.res /Fe:Visor3rdGen_GenGCODE.exe
+cl Visor3rdGenGenGCODE.cpp Visor3rdGenMain.cpp /D LANG_FR /link resource.res /Fe:Visor3rdGen_GenGCODE_FR.exe
 del *.obj *.res
