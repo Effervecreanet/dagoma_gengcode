47059b3e33d8f30c40c6c14571d3d52264869015
Franck Lesage <140308388+Effervecreanet@users.noreply.github.com>
2026-06-02 13:33:10 +0200

    Add visor 4

--- visor_4thgen/cmdUI/make_en.bat
+++ visor_4thgen/cmdUI/make_en.bat
@@ -2,5 +2,5 @@
 REM This script should be run in x64 Native Tools Command Prompt
 REM You have to run "rc resource.rc" to have "resource.res" file
 REM rc resource.rc
-cl Visor3rdGenGenGCODE.cpp Visor3rdGenMain.cpp resource.res /Fe:VisorWithGlass_dagoma0_g.exe
+cl Visor4rdGenGenGCODE.cpp Visor4rdGenMain.cpp resource.res /Fe:VisorWithGlass_dagoma0_g.exe
 del *.obj *.res
